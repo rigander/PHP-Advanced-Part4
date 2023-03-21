205c1ae59fa8fcbc9f75165ef33b448ccd7f21d0
Andrey Komov <andrewkomov@gmail.com>
2023-03-21 21:17:53 +0200

    generator

--- demo/spl/Generator.php
+++ demo/spl/Generator.php
@@ -3,14 +3,36 @@
 // за исключением того, что вместо возвращения одного значения,
 // генератор будет перебирать столько значений, сколько необходимо.
 // Любая функция, содержащая yield, является функцией генератора.
-function nums(){
-    echo "START<br>";
+//function nums(){
+//    echo "START<br>";
+//
+//    for($i=0; $i<5; ++$i){
+//        yield $i;
+//    }
+//
+//    echo "FINISH<br>";
+//}
+//foreach (nums() as $v)
+//    echo "Value: $v <br>";
 
-    for($i=0; $i<5; ++$i){
-        yield $i;
+//function gen(){
+//    yield "a";
+//    yield "b";
+//    yield 10 => "Hello";
+//    yield "e";
+//}
+//
+//foreach (gen() as $k=>$v) {
+//    echo "$k : $v<br>";
+//
+//}
+
+function echoLogger(){
+    while(true){
+        echo "Log: " . yield . "<br>";
     }
-
-    echo "FINISH<br>";
 }
-foreach (nums() as $v)
-    echo "Value: $v <br>";
+
+$logger = echoLogger();
+$logger->send('Hello');
+$logger->send('World');
