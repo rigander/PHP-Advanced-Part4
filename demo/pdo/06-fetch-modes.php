eb1a1cadb5427c6ac20b014e435456be7eac7a98
Andrey Komov <andrewkomov@gmail.com>
2023-03-24 04:25:39 +0200

    PDO fetch

--- demo/pdo/06-fetch-modes.php
+++ demo/pdo/06-fetch-modes.php
@@ -1,12 +1,18 @@
 <?php
+//todo PDOStatement::fetch — Извлечение следующей строки
+// из результирующего набора.
+// fetch_assoc — Выбирает следующую строку из набора
+// результатов и помещает её в ассоциативный массив.
 
 $db = new PDO("sqlite:users.db");
-
 $sql = "SELECT * FROM user";
 
 $stmt = $db->query($sql);
 
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
+//$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$result = $stmt->fetch(PDO::FETCH_NUM);
+//$result = $stmt->fetch(PDO::FETCH_BOTH);
+
 
 
 foreach($result as $key=>$val){
