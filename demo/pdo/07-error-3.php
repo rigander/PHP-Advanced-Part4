fdfbb9eb727af1efa890b93a98ba5f5fa0d67b50
Andrey Komov <andrewkomov@gmail.com>
2023-03-24 04:39:51 +0200

    error silent

--- demo/pdo/07-error-3.php
+++ demo/pdo/07-error-3.php
@@ -3,7 +3,14 @@ try {
   $db = new PDO("sqlite:users.db");
 
   echo 'Connected to database<br>';
-
+    //todo ERRMODE_SILENT - PDO просто предоставит вам код ошибки,
+    // который можно получить методами PDO::errorCode() и PDO::errorInfo().
+    // Эти методы реализованы как в объектах запросов, так и в объектах баз
+    // данных. Если ошибка вызвана во время выполнения кода объекта запроса,
+    // нужно вызвать метод PDOStatement::errorCode() или
+    // PDOStatement::errorInfo() этого объекта.
+    // Если ошибка вызова объекта базы данных, нужно вызвать аналогичные
+    // методы у этого объекта.
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
   $sql = "SELECT username FROM user";
