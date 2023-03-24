fb318f63088a31737c9b0a6cd265ae43f342cefd
Andrey Komov <andrewkomov@gmail.com>
2023-03-24 02:55:49 +0200

    PDO connect(), exec()

--- demo/pdo/02-connect.php
+++ demo/pdo/02-connect.php
@@ -1,2 +1,18 @@
 <?php
+//todo PDO - Php Data Object.
+// позволяет работать с СУБД через методы класса PDO.
+// Выбор типа СУБД и файла БД. Все параметры можно подключать через ini
+// конфигурационный файл.
+// Например $params = parse_ini_file("config.ini");
+// $db = new PDO($params["db.com"],
+//              $params["db.user"],
+//              $params["db.pass"]);
+
+
 $dbh = new PDO("sqlite:test.db");
+
+//todo  Для безответных запросов теперь доступен метод exec().
+// В случае успеха exec вернет количество строк над которыми было произведено
+// изменение.
+$count = $dbh->exec("INSERT INTO user(name, email) 
+VALUES ('John', 'john@gmail.com')");
--- demo/pdo/03-exec.php
+++ demo/pdo/03-exec.php
@@ -2,15 +2,17 @@
 
   $db = new PDO("sqlite:users.db");
     
-/*
+
   //INSERT
-  $count = $db->exec("INSERT INTO user(name, email) VALUES ('John', 'john@gmail.com')");
+  $count = $db->exec("INSERT INTO user(name, email) 
+VALUES ('John', 'john@gmail.com')");
 
   echo $count;
-*/
-/*
+
+
   //UPDATE
-  $count = $db->exec("UPDATE user SET email='john@mail.ru' WHERE name='John'");
+  $count = $db->exec("UPDATE user SET email='john@mail.ru' 
+            WHERE name='John'");
 
   echo $count;
-*/
+
