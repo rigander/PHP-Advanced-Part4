991bd22e7214dd35bca3ae7b26a9ffbb7c44f2ec
Andrey Komov <andrewkomov@gmail.com>
2023-03-29 21:53:45 +0300

    cURL

--- demo/curl/01-simple.php
+++ demo/curl/01-simple.php
@@ -1,10 +1,15 @@
 <?php
+//todo Простейший пример cURL
+
   require "_header.php";
-
+//todo Инициализация сURL
   $curl = curl_init();
-
+//todo curl_setopt — Устанавливает параметр для сеанса CURL
+// CURLOPT_URL - Загружаемый URL. Данный параметр может быть также
+// установлен при инициализации сеанса с помощью curl_init().
+// HOST_NAME . "test.php" - сам URL в виде конкатенации константы и имени файла.
   curl_setopt($curl, CURLOPT_URL, HOST_NAME . "test.php");
-
+//todo curl_exec — Выполняет стандартный запрос cURL (что то типа file_get_contents)
   curl_exec($curl);
-
+//todo Закрыть. curl_close — Завершает сеанс cURL.
   curl_close($curl);
--- demo/curl/test.php
+++ demo/curl/test.php
@@ -1,2 +1,8 @@
 <?php
-  echo "<h1>Hello, cURL!</h1>";
+  echo "<body style='
+                color: wheat;
+                background: rgb(33,33,169);
+                background: linear-gradient(0deg, rgb(76,76,162) 1%,
+                rgb(33,47,67) 36%, rgb(54,76,110) 100%);'>
+                <h1>Hello, cURL!</h1>
+        </body>";
