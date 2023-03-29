ad1d3ee80a7538bfad543deae7ce2348c1adf59f
Andrey Komov <andrewkomov@gmail.com>
2023-03-30 02:32:40 +0300

    curl 02-CURLOPT_RETURNTRANSFER

--- demo/curl/02-RETURNTRANSFER.php
+++ demo/curl/02-RETURNTRANSFER.php
@@ -1,5 +1,11 @@
 <?php
   require "_header.php";
+  //todo К примеру я хочу перед тем как отдать данные браузеру обработать их.
+  // то есть поместить результат функции curl_exec() в переменную.
+  // Для этого нужно использовать параметр - CURLOPT_RETURNTRANSFER.
+  // CURLOPT_RETURNTRANSFER - true (1) для возврата результата передачи
+  // в качестве строки из curl_exec() вместо прямого вывода в браузер.
+
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, HOST_NAME . "test.php");
 
@@ -7,4 +13,7 @@
 
   $result = curl_exec($curl);
   curl_close($curl);
+
+  //todo В результате я могу делать с полученными данными все что угодно,
+  // перед тем как вывести их браузер.
   print $result;
