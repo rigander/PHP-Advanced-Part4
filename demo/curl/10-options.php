1f4f8ce2192fbae086cae89c2a81f5abac7a6e03
Andrey Komov <andrewkomov@gmail.com>
2023-04-01 06:32:52 +0300

    options

--- demo/curl/10-options.php
+++ demo/curl/10-options.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+//todo Есть заголовки очень часто используемые. Соответственно под самые известные
+// заголовки существуют свои соответствующие опции.
+// CURLOPT_USERAGEN - Содержимое заголовка "User-Agent: ", посылаемого
+// в HTTP-запросе.
   require "_header.php";
   $curl = curl_init(); 
   curl_setopt($curl, CURLOPT_URL, HOST_NAME . "alloptions.php"); 
